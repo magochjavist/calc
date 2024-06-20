54c84e9c3d7cd34a0298b3dd5421d5cbd3acc090
magochjavist <Bonogrse@rambler.ru>
2024-06-20 18:41:39 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
-class Main {
+import java.util.Scanner;
+
+public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
